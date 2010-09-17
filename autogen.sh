d168822e4766194349c6bd4f5b21ce69e6e53d61
Florian Haas <florian.haas@linbit.com>
2010-09-17 15:43:05 +0200

    Add autogen hack to generate configure.ac from configure.ac.stub
    Scan subdirectories for Makefile.am, and add them to
    AC_CONFIG_FILES list automatically.

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,13 @@
 # Run this to generate all the initial makefiles, etc.
 set -e
 
+AMFILES=`find -name Makefile.am -printf '%p ' | sed -e 's,\./\([^ ]*\)\.am ,\1 ,g'`
+
+cat configure.ac.stub - > configure.ac <<EOF
+AC_CONFIG_FILES([$AMFILES])
+AC_OUTPUT
+EOF
+
 mkdir -p m4
 automake --add-missing --include-deps --copy
 autoconf
