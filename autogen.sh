44d6e147bcd976c656c5eaa31cdd153bef7d96ba
Florian Haas <florian.haas@linbit.com>
2010-09-20 18:28:54 +0200

    Honor autoconf substitution variables in FOP config and FO stylesheets

--- autogen.sh
+++ autogen.sh
@@ -2,10 +2,11 @@
 # Run this to generate all the initial makefiles, etc.
 set -e
 
-AMFILES=`find -name Makefile.am -printf '%p ' | sed -e 's,\./\([^ ]*\)\.am ,\1 ,g'`
+AMFILES=`find -name 'Makefile.am' -printf '%p ' | sed -e 's,\./\([^ ]*\)\.am ,\1 ,g'`
+INFILES=`find -name '*.in' -not -name 'Makefile.in' -printf '%p ' | sed -e 's,\./\([^ ]*\)\.in ,\1 ,g'`
 
 cat configure.ac.stub - > configure.ac <<EOF
-AC_CONFIG_FILES([$AMFILES])
+AC_CONFIG_FILES([$AMFILES $INFILES])
 AC_OUTPUT
 EOF
 
